6aced27668beb56bca83c9350b389fe895eb2fb7
Vladimir <kobzarenkovolodymyr@gmail.com>
2019-08-29 11:21:29 +0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+console.log('Client side javascript file is loaded!')
+
+
+// принести, забрать
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input') 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+// messageOne.textContent = 'from js'
+
+// Form
+weatherForm.addEventListener('submit', (e) => {
+    // prevent reload page
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://127.0.0.1:3000/weather?address=' + location).then((response)=>{
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast             
+            }
+           
+        })
+    })
+})
